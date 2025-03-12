266cee009b49403ba1a38c428b75b73aa4561f21
Kahsauce <adrien.wyer@gmail.com>
2025-03-12 11:29:50 +0100

    Ajout automatique de dé.py par GPT

--- dé.py
+++ dé.py
@@ -2,14 +2,18 @@ import random
 import sys
 import unittest
 
+def random_even_number():
+    return random.choice(range(2, 101, 2))
+
 def run_app():
-    print("Nombre aléatoire entre 1 et 100:", random.randint(1, 100))
+    print("Résultat du numéro pair (1-100) :", random_even_number())
 
 class TestRandomNumber(unittest.TestCase):
-    def test_random_number_in_range(self):
-        num = random.randint(1, 100)
+    def test_random_even_number_in_range_and_even(self):
+        num = random_even_number()
         self.assertGreaterEqual(num, 1)
         self.assertLessEqual(num, 100)
+        self.assertEqual(num % 2, 0)
 
 if __name__ == "__main__":
     if '--test' in sys.argv:
